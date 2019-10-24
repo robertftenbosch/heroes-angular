5b0fbdb7809bafbbb0f0586369bdc2e5462c0bb2
robert ten bosch <robert.ten.bosch@kiwa.com>
2019-10-24 08:52:10 +0200

    update detail component en routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero: Hero = { id: 1, name: 'Windstorm'};
   heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    console.log(hero);
-    this.selectedHero = hero;
-  }
-
   constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
+    console.log("getheroes");
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
